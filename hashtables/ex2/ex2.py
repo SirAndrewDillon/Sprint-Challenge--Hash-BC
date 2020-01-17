a7cade5e30b6b7e0804ec4d16a7d239383ce3aa8
Andrew Dillon <majik895@gmail.comC:/django-projects/Scripts/python.exe>
2020-01-17 14:38:14 -0500

    mvp complete

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -15,9 +15,18 @@ class Ticket:
 def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
     route = [None] * length
+    current_destination = None
 
-    """
-    YOUR CODE HERE
-    """
+    for i in range(length):
+        hash_table_insert(
+            hashtable, tickets[i].source, tickets[i].destination)
+        if tickets[i].source == "NONE":
+            current_destination = tickets[i].destination
+            route[0] = current_destination
 
+    for i in range(1, length):
+        next_destination = hash_table_retrieve(
+            hashtable, current_destination)
+        route[i] = next_destination
+        current_destination = next_destination
     return route
